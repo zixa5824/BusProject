906229cc64def86663bb3a3f6d7bbbeb205182e7
Yahia Ashraf <yahia_ashraf_sabri2@hotmail.com>
2019-03-18 14:16:50 +0200

    Add files via upload

---
+++ src/bus/Main.java
@@ -0,0 +1,28 @@
+package bus;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("MainScene.fxml"));
+        Scene mainScene = new Scene(root, 600, 400);
+
+        primaryStage.setTitle("Bus Station");
+        primaryStage.setScene(mainScene);
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
